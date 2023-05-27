c53c26959a8daf21ad232b2367433a0f6e8c2774
MikeKimutai <mikekimutai14@gmail.com>
2023-05-27 09:05:56 +0300

    adding links to website projects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var clickCount = 0
 const header = document.querySelector("header");
 window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky", window.scrollY > 0);
@@ -9,14 +10,24 @@ document.getElementById("Date").innerHTML = getDate;
 var navlist = document.getElementById("menus");
 var close = document.getElementById("close");
 
-navlist.addEventListener("click",function(){
-  document.getElementById("navlist").style.display = "block";
-  document.getElementById("menus").style.display = "none";
-  document.getElementById("close").style.display = "block";
+navlist.addEventListener("click", function(){
+  clickCount ++;
+  if (clickCount == 1){
+    document.getElementById("navlist").style.display = "block";
+  } else if(clickCount==2)
+  {
+    document.getElementById("navlist").style.display = "none";
+    clickCount = 0
+  }
 })
+// navlist.addEventListener("click",function(){
+//   document.getElementById("navlist").style.display = "block";
+//   document.getElementById("menus").style.display = "none";
+//   document.getElementById("close").style.display = "block";
+// })
 
-close.addEventListener("click",function(){
-  document.getElementById("navlist").style.display = "none";
-  document.getElementById("menus").style.display = "block";
-  document.getElementById("close").style.display = "none";
-})
+// close.addEventListener("click",function(){
+//   document.getElementById("navlist").style.display = "none";
+//   document.getElementById("menus").style.display = "block";
+//   document.getElementById("close").style.display = "none";
+// })
